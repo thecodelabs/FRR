73f5f43a8f4ab7630350dfb8d750613d9ea3db95
Petermuturi <pmuturi65@gmail.com>
2017-01-11 18:06:52 +0300

    setup Home Component

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import RecipeList from './recipes/RecipeList'
+
+import { Divider } from 'semantic-ui-react'
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+        <Divider/>
+        <RecipeList {...this.props}/>
+      </div>
+    )
+  }
+}
+
+export default Home
