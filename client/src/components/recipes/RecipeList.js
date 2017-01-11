f71fd6c9226891ff087e8197ac114126a3f24c7b
Petermuturi <pmuturi65@gmail.com>
2017-01-11 18:14:53 +0300

    setup recipeList to pass props to RecipeCard

---
+++ client/src/components/recipes/RecipeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Card } from 'semantic-ui-react'
+import RecipeCard from './RecipeCard'
+
+class RecipeList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props.recentRecipes();
+  }
+
+  render() {
+    return(
+      <Card.Group itemsPerRow={5}>
+        {this.props.recipes.map((recipe, i)=> <RecipeCard {...this.props} key={i} recipe={recipe} />)}
+      </Card.Group>
+    )
+  }
+}
