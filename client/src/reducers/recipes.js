71bdf0fdd3e5e2066ad1ffb6d112da3713c67884
Petermuturi <pmuturi65@gmail.com>
2017-01-11 17:33:37 +0300

    setup recipe reducer with default state of empty array

---
+++ client/src/reducers/recipes.js
@@ -0,0 +1,11 @@
+recipes(state=[], action)=> {
+  switch (action.type) {
+    case "RECENT_RECIPES_SUCCEEDED":
+      return action.recipes;
+    default:
+      return state;
+  }
+
+}
+
+export default recipes;
