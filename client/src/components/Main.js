b1f4d4c1627d2cc18fcfc0ccfee608bafbcb00ee
Petermuturi <pmuturi65@gmail.com>
2017-01-11 17:59:56 +0300

    setup Main Component - renders other components

---
+++ client/src/components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import { Link } from 'react-router'
+import { Header,  Container } from 'semantic-ui-react'
+
+class Main extends Component {
+  render(){
+    return(
+      <Container>
+        <Header as="h1" textAlign="center">
+          <Link to="/">Recipe test</Link>
+        </Header>
+        {React.cloneElement(this.props.children, this.props)}
+      </Container>
+    )
+  }
+}
