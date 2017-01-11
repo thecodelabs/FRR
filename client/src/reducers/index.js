d2c1602974c8f1adcc8106c92284461da0bd7d3c
Petermuturi <pmuturi65@gmail.com>
2017-01-11 17:48:18 +0300

    setup api imported in the sagas

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,3 +2,7 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
 import recipes from './recipes'
+
+const rootReducer = combineReducers({recipes, routing: routerReducer});
+
+export default rootReducer;
---
+++ client/src/services/api.js
@@ -0,0 +1,4 @@
+export getRecentRecipes(app)=> {
+  const recipes = app.service('recipes');
+  return recipes.get().then((data, err)=> data.data);
+}
