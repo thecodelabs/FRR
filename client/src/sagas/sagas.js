869e95aa7439e1419e2250f4195e7aa0d77f54c5
Petermuturi <pmuturi65@gmail.com>
2017-01-11 17:30:06 +0300

    set up sagas file

---
+++ client/src/sagas/sagas.js
@@ -0,0 +1,20 @@
+import { takeEvery } from 'redux-sagas'
+import { fork, call, put } from 'redux-sagas/effects'
+import { getRecentRecipes } from '../services/api'
+import { browserHistory } from 'react-router'
+
+
+function* fetchRecentRecipes(feathersApp) {
+  const recipes = yield call(getRecentRecipes, feathersApp);
+  yield put({type: "RECENT_RECIPES_SUCCEEDED", recipes});
+}
+
+function* recentRecipesSaga(feathersApp) {
+  yield* takeEvery("RECENT_RECIPES_REQUESTED", fetchRecentRecipes, feathersApp);
+}
+
+export default function* root(feathersApp) {
+  yield [
+    fork(recentRecipesSaga , feathersApp)
+  ]
+}
