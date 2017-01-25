4fe5ddfcb450301a58cc8ba54c3aa2762c77319b
Petermuturi <pmuturi65@gmail.com>
2017-01-25 19:40:13 +0300

    changed to find

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
 export function getRecentRecipes(app) {
-  const recipes = app.service('recipes');
-  return recipes.get().then((data, err)=> data.data);
+  const recipes = app.service('recipe_tests');
+  return recipes.find().then((data, err)=> data.data);
 }
